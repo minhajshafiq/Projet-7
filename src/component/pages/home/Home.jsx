851fdd2c376bcc82057b5e7ebfd9c860ebd6ff62
minhajshafiq <minhaj93500@gmail.com>
2023-02-04 22:39:06 +0100

    Mise à jour du SCSS HeroBanner

--- src/component/pages/home/Home.jsx
+++ src/component/pages/home/Home.jsx
@@ -5,14 +5,12 @@ import HeroBanner from '../../heroBanner/HeroBanner.jsx';
 
 function Home() {
   return (
-    <div>
       <main>
         <HeroBanner />
         <div className='CardContainer'>
             <LocationsListCard />
         </div>
       </main>
-    </div>
   )
 }
 
