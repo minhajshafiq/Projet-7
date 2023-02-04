18d46b88653b3d70408ab84d0320f448eca04b96
minhajshafiq <minhaj93500@gmail.com>
2023-02-05 00:19:14 +0100

    Amélioration page Error

--- src/component/pages/error/Error.jsx
+++ src/component/pages/error/Error.jsx
@@ -5,8 +5,10 @@ function Error() {
     return (
         <main className="error">
             <h1 className="error_title"> 404 </h1>
-            <p className="error_p"> Oups! La page que vous demandez n'existe pas. </p>
-            <Link className="error_link" to="/">Retourner sur la page d’accueil</Link>
+            <div className="error_text">
+                <p className="error_p"> Oups! La page que vous demandez n'existe pas. </p>
+                <Link className="error_link" to="/">Retourner sur la page d’accueil</Link>
+            </div>
         </main>
     )
 }
