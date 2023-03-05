d5b738444f936637d5c07c43e5e14a87809f2985
minhajshafiq <minhaj93500@gmail.com>
2023-03-05 06:39:22 +0100

    Ajout d'animation SCSS

--- src/component/Collapse/Collapse.jsx
+++ src/component/Collapse/Collapse.jsx
@@ -8,11 +8,8 @@ function Collapse({ title, description }) {
     <div id="Collapse">
       <button onClick={() => setOpen(!open)}>
         <h2>{title}</h2>
-        <svg className="angle" viewBox="0 0 24 24">
-          {open
-            ? <path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z" fill="#fff"/>
-            : <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z" fill="#fff"/>
-          }
+        <svg className={`angle ${open ? 'angle_up' : 'angle_down'}`} viewBox="0 0 24 24">
+          <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z" fill="#fff"/>
         </svg>
       </button>
       {open && <p>{description}</p>}
